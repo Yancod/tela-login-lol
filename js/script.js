863e9f8e8c16a8212a20b80bd58e9bada6c401d0
Yancod <yanchanceller@gmail.com>
2022-11-01 14:34:22 -0300

    Criação da logica do botão de login

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll(".input");
+const button = document.querySelector('.login__button');
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
@@ -10,7 +11,17 @@ const handleFocusOut = ({ target }) => {
         span.classList.remove('input__active');
     }
 }
+const handleChange = () => {
+    const [username, password] = inputs;
+
+    if (username.value && password.value.length >= 8) {
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', '');
+    }
+}
 
 
 inputs.forEach((input) => input.addEventListener("focus", handleFocus));
-inputs.forEach((input) => input.addEventListener("focusout", handleFocusOut))
+inputs.forEach((input) => input.addEventListener("focusout", handleFocusOut));
+inputs.forEach((input) => input.addEventListener("input", handleChange));
